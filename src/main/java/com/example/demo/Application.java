8d833ca272f5d6e3b5f36eeccaa928f2d737f6e7
Chandra Sekhar <71069304+vennapusa0383@users.noreply.github.com>
2024-01-09 18:14:00 +0530

    Update Application.java

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
